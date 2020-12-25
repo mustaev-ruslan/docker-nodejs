1aafd6ff5d4ab35abb9c55f80259152c6816bffc
Ruslan Mustaev <mustaev.ruslan@hotmail.com>
2020-12-25 10:10:24 +0300

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const makeApiRequest = () => {
+	  axios("/api/testwithcurrentuser").then(response => {
+		  console.log('response', response);
+	  });
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+	  <button onClick={makeApiRequest}>Make api request</button>
+    </div>
+  );
+}
+
+export default App;
